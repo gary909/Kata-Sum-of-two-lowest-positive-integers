4fb26bb9d4c1eee4ef496909bf36048f108f2765
gary909 <garywhite61@hotmail.com>
2022-08-16 13:05:30 +0200

    code working

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 function sumTwoSmallestNumbers(numbers) {
-    //Code here
+    numbers.sort(function (a, b) {
+        return a - b;
+    });
+    return numbers[0] + numbers[1];
 }
 
 console.log(sumTwoSmallestNumbers([5, 8, 12, 19, 22])); // 13
